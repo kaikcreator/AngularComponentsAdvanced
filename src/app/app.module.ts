833ba0a2ea8570ac239a14ec6358c749b00b7564
-KaiK- <enriqueoriol@gmail.com>
2017-05-03 18:34:20 +0200

    added timer empty component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TimerComponent } from './timer/timer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    TimerComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.sass']
+})
+export class TimerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
