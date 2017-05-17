3ac0a38dbc1b9bdad16d46c092be29750add775e
-KaiK- <enriqueoriol@gmail.com>
2017-05-17 13:26:26 +0200

    9 - Stateless components

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,13 +1,26 @@
-import { Component, Input, Output, OnInit} from '@angular/core';
+import { Component, Input, Output, OnInit, OnChanges} from '@angular/core';
 
 @Component({
   selector: 'app-display',
   templateUrl: 'display.component.html',
   styleUrls: ['./display.component.scss']
 })
-export class DisplayComponent{
+export class DisplayComponent implements OnChanges{
 
   @Input() time:number = null;
+  public minutes:string = "00";
+  public seconds:string = "00";
+
   constructor() { }
+
+  ngOnChanges(changes){
+    if(changes.time){
+      const minutes = Math.trunc(changes.time.currentValue / 60);
+      const seconds = changes.time.currentValue - minutes*60;
+
+      this.minutes = ("0" + minutes).substr(-2);
+      this.seconds = ("0" + seconds).substr(-2);
+    }
+  }
 
 }
