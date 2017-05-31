5fd0d1b0a15a9a3ed0fb98a7a5af9c337de3d1db
-KaiK- <enriqueoriol@gmail.com>
2017-05-31 19:15:24 +0200

    18 - ng-content and projection slots

--- src/app/alert-view/alert-view.component.ts
+++ src/app/alert-view/alert-view.component.ts
@@ -7,10 +7,6 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class AlertViewComponent implements OnInit {
 
-  @Output() onCancel = new EventEmitter<void>();
-  @Output() onSubmit = new EventEmitter<number>();
-
-
   constructor() { }
 
   ngOnInit() {
