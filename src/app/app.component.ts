578a8af4c734825c61f500e54772b4c6ea6c465e
-KaiK- <enriqueoriol@gmail.com>
2017-05-31 19:27:59 +0200

    19 - ng-content and DRY strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public isAddTimerVisible:boolean = false;
+  public isEndTimerAlertVisible:boolean = false;
   public time:number = 0;
+  public timers:Array<number> = [];
 
-  constructor() { }
+  constructor() { 
+    this.timers = [3, 20, 185];
+  }
 
   logCountdownEnd(){
     console.log("the countdown has finished");
@@ -21,6 +25,19 @@ export class AppComponent {
 
   public hideAddTimer(){
     this.isAddTimerVisible = false;
+  }
+
+  public showEndTimerAlert(){
+    this.isEndTimerAlertVisible = true;
+  }
+
+  public hideEndTimerAlert(){
+    this.isEndTimerAlertVisible = false;
+  }  
+
+  public submitAddTimer(){
+    this.timers.push(this.time);
+    this.hideAddTimer();
   }
 
 }
