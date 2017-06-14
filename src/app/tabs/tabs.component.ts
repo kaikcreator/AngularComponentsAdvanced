61b16c8598f51e37c26cc7dcfc648417d8ad4936
-KaiK- <enriqueoriol@gmail.com>
2017-06-14 15:27:51 +0200

    22 - @ContentChildren and QueryLists: access transcluded children from parent

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ContentChild, AfterContentInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, ContentChildren, QueryList, AfterContentInit, OnDestroy } from '@angular/core';
 import { TabComponent } from "app/tab/tab.component";
 import { Tab } from "../tab/tab.interface";
 
@@ -10,10 +10,8 @@ import { Tab } from "../tab/tab.interface";
 })
 export class TabsComponent implements OnInit, AfterContentInit, OnDestroy {
 
-  @ContentChild(TabComponent) tab:TabComponent;
-
-  public tabs:Tab[] = [];
-  private tabClickSubscription:any;
+  @ContentChildren(TabComponent) public tabs:QueryList<TabComponent>;
+  private tabClickSubscriptions:any[] = [];
 
   constructor() { }
 
@@ -21,31 +19,26 @@ export class TabsComponent implements OnInit, AfterContentInit, OnDestroy {
   }
 
   ngOnDestroy(){
-    if(this.tabClickSubscription){
-      this.tabClickSubscription.unsubscribe();
+    if(this.tabClickSubscriptions){
+      this.tabClickSubscriptions.forEach(
+        item => item.unsubscribe()
+      );
     }
   }
 
   ngAfterContentInit(){
-    if(this.tab){
-      console.log(this.tab);
-      this.addTab(this.tab);
-      this.tabClickSubscription = this.tab.onClick.subscribe(()=>{console.log("tab content click detected");});
-    }
-
-  }
-
-  addTab(tab:Tab){
-    if (this.tabs.length === 0) {
-      tab.isActive = true;
-    }
-    this.tabs.push(tab);
+    console.log(this.tabs);
+    this.tabs.forEach(tab =>{
+      let subscription = tab.onClick.subscribe(()=>{
+        console.log(`tab ${tab.title} content clicked`);
+      });
+      this.tabClickSubscriptions.push(subscription);
+    });
+    this.selectTab(this.tabs.first);
   }
 
   selectTab(tab:Tab) {
-    for (let tab of this.tabs){
-      tab.isActive = false;
-    }
+    this.tabs.forEach(tab => tab.isActive = false);
     tab.isActive = true;
   }
   
